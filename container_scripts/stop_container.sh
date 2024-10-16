414b0a999c10c0e4c95f28f9a3bbcfce3b81a863
Krushna Sonawane <105139116+Krish9130@users.noreply.github.com>
2024-10-16 23:13:39 +0530

    update

--- container_scripts/stop_container.sh
+++ container_scripts/stop_container.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
+# Get the container ID or name running on port 5000
+container_id=$(docker ps -q --filter "publish=5000")
 
-# Stop the running container (if any)
-echo "Hi"
+# Check if a container is found
+if [ -n "$container_id" ]; then
+    echo "Stopping and removing container: $container_id"
+    docker stop "$container_id"
+    docker rm "$container_id"
+else
+    echo "No container is running on port 5000."
+fi
